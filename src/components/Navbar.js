37e0d51510e21555f5a0af6b788f21f018a60ec7
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-08-31 14:48:42 -0700

    made the navbar dynamically stick when scrolling down past the header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,25 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import styled from 'styled-components'
 
-function Navbar(){
+function Navbar(props){
+    const [stick, setStick] = useState(false);
+
+    //useRef is called on page load to grab the offsetTop of the navbar. Which will be used to determine whether or not it should be sticky. 
+    const inputRef = useRef()
+    useEffect(() => {
+        const { offsetTop } = inputRef.current
+        window.addEventListener('scroll', () => {
+            window.pageYOffset > offsetTop ? setStick(true) : setStick(false)
+        })
+    }, [])
+    
     return(
-    <NavBar>
+    <NavBar ref={inputRef} stick={stick}>
         <NavButton>Languages</NavButton>
         <NavButton>Projects</NavButton>
         <NavButton>About Me</NavButton>
         <NavButton>Contact Me</NavButton>
-    </NavBar>      
+    </NavBar>
 )}
 
 const NavBar = styled.nav`
@@ -20,7 +31,7 @@ const NavBar = styled.nav`
     border-radius: 5px 5px 0px 0px;
     padding: 1.5% 0%;
     background-color: #F7F7F7;
-    position: relative;
+    position: ${props => props.stick ? "fixed" : "relative"}
 `
 
 const NavButton = styled.button`
