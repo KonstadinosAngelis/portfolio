398079917c6e937ea4c8781fca0b708f081ab251
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-09-02 10:19:07 -0700

    added the projects component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Navbar from './components/Navbar'
 import Languages from './components/Languages'
+import Projects from './components/Projects'
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Navbar />
         <Languages />
+        <Projects />
     </div>
   );
 }
