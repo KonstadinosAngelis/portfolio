76b7730a97a0cc56d8a70326f24beee1933674a7
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-09-29 13:21:44 -0700

    styled project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,24 +7,33 @@ function Projects(){
             <HeaderText>My Projects</HeaderText>
             <ProjectsWrapper>
                 <ProjectCard>
-                    <ProjectName>Game of Life</ProjectName>
-                    <ProjectDescrip>Description of the game of life</ProjectDescrip>
-                    <ProjectButton>Github</ProjectButton>
-                    <ProjectButton>Deploy</ProjectButton>
+                    <ProjectImg href=""/> 
+                    <ProjectName>SFMTA Data Anylsis</ProjectName>
+                    <ProjectDescrip>A web app created to help agency members identify problems with San Franciscos public transportation</ProjectDescrip>
+                    <ButtonWrapper>
+                        <ProjectButton>Github</ProjectButton>
+                        <ProjectButton>Deploy</ProjectButton>
+                    </ButtonWrapper>
                 </ProjectCard>
 
                 <ProjectCard>
+                    <ProjectImg href=""/> 
                     <ProjectName>Game of Life</ProjectName>
-                    <ProjectDescrip>Description of the game of life</ProjectDescrip>
-                    <ProjectButton>Github</ProjectButton>
-                    <ProjectButton>Deploy</ProjectButton>
+                    <ProjectDescrip>A faithful recreation of the computer science classic Conway's game of life. </ProjectDescrip>
+                    <ButtonWrapper>
+                        <ProjectButton>Github</ProjectButton>
+                        <ProjectButton>Deploy</ProjectButton>
+                    </ButtonWrapper>
                 </ProjectCard>
                 
                 <ProjectCard>
-                    <ProjectName>Game of Life</ProjectName>
-                    <ProjectDescrip>Description of the game of life</ProjectDescrip>
-                    <ProjectButton>Github</ProjectButton>
-                    <ProjectButton>Deploy</ProjectButton>
+                    <ProjectImg href=""/> 
+                    <ProjectName>Potluck Planner</ProjectName>
+                    <ProjectDescrip>A backend designed to supply user inputted data to organize potlucks</ProjectDescrip>
+                    <ButtonWrapper>
+                        <ProjectButton>Github</ProjectButton>
+                        <ProjectButton>Deploy</ProjectButton>
+                    </ButtonWrapper>
                 </ProjectCard>
                 
             </ProjectsWrapper>
@@ -40,11 +49,11 @@ const Wrapper = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    color: blue;
 `
 
 const HeaderText = styled.h2`
     color: #636363;
+    padding: 2% 0%;
 `
 
 const ProjectsWrapper = styled.div`
@@ -55,22 +64,38 @@ const ProjectsWrapper = styled.div`
 
 const ProjectCard = styled.div`
     width: 25%;
-    border: 1px solid gray;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border: 2px solid gray;
     text-align: center;
-    justify-content: center;
     margin: 1% 0%;
     padding: 1% 1%;
 `
 
+const ProjectImg = styled.img`
+    width: 250px;
+`
+
 const ProjectName = styled.h3`
-    color: red;
+    color: #636363;
 `
 
 const ProjectDescrip = styled.p`
-    color: green
+    color: black;
 `
 
+const ButtonWrapper = styled.div`
+
+`
 const ProjectButton = styled.button`
-    border: 1px solid purple;
+    border: 0px solid gray;
+    margin: 0% 4%;
+    background-color: white-smoke;
+
+    &:hover {
+        background-color: gainsboro;
+        text-decoration: underline;
+    }
 `
 export default Projects
