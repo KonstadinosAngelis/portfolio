4465cc2442dbcee71381c888b62529d4d0a87f44
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-10-06 10:08:41 -0700

    worked on footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const Footer = () => {
     return(
         <Wrapper>
-            <FooterText></FooterText>
+            <FooterText> </FooterText>
         </Wrapper>
 )}
 
