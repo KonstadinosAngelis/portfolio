1468ae7324394fc3547aa634669cf87c2e0283e5
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-10-12 14:01:24 -0700

    more footer work

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,13 +15,6 @@ const Wrapper = styled.footer`
     flex-direction: column;
     align-items: center;
     `
-    
-const footerWrapper = style.div`
-    color: #636363;
-    font-size: 1.5rem;
-    display: flex;
-    justify-content: center;
-`
 
 const FooterText = styled.h2`
     color: #636363;
